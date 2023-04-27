26900f3fc01b2fda9a21e09141e47018b56137f2
solar025 <128208994+solar025@users.noreply.github.com>
2023-04-27 23:56:26 +0300

    Первое задание

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+void EvenNatural(int first, int second)
+{
+    if(first > second)
+    {
+        return;
+    }
+    if(first%2 == 0)
+System.Console.WriteLine(first);
+EvenNatural(first + 1, second);
+}
+EvenNatural(1,6);
