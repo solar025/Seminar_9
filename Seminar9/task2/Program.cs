627835f611a72ed097d30d44c2ba40bc7c37ab3d
John Doe <ola_soldattova@mail.ru>
2023-04-27 23:52:08 +0300

    Первое задание

---
+++ Seminar9/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int sumNatural(int first, int second)
+{
+    if(first > second)
+    {
+        return 0;
+    }
+return (first + sumNatural(first + 1, second));
+}
+System.Console.WriteLine(sumNatural(5,8));
